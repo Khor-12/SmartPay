a00c24e7eeb57fa7e7e56660bbedf2fc24320b2e
Host Hub <khor@g>
2023-08-08 11:50:31 +0300

    Implement some ui features

--- app/src/main/java/com/khor/smartpay/feature_home/HomeScreen.kt
+++ app/src/main/java/com/khor/smartpay/feature_home/HomeScreen.kt
@@ -1,15 +1,19 @@
 package com.khor.smartpay.feature_home
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -39,15 +43,17 @@ import com.khor.smartpay.feature_home.components.PaymentButton
 
 @Composable
 fun HomeScreen() {
-    StandardToolbar(title = { Text("+256787102643", style = MaterialTheme.typography.titleMedium)}, navActions = {
-        IconButton(
-            onClick = {
+    StandardToolbar(
+        title = { Text("+256787102643", style = MaterialTheme.typography.titleMedium) },
+        navActions = {
+            IconButton(
+                onClick = {
 
-            }, modifier = Modifier
-        ) {
-            Icon(imageVector = Icons.Filled.Settings, contentDescription = "Settings")
-        }
-    })
+                }, modifier = Modifier
+            ) {
+                Icon(imageVector = Icons.Filled.Settings, contentDescription = "Settings")
+            }
+        })
 
     Column(
         modifier = Modifier
@@ -60,7 +66,7 @@ fun HomeScreen() {
                     .alpha(0.7f)
                     .align(Alignment.CenterHorizontally)
                     .padding(top = 12.dp),
-                text = "Balance",
+                text = "Total Balance",
                 style = MaterialTheme.typography.bodyMedium,
                 textAlign = TextAlign.Center,
                 fontWeight = FontWeight.Medium
@@ -105,9 +111,88 @@ fun HomeScreen() {
             )
         }
         Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "Analytics", style = MaterialTheme.typography.titleLarge, textAlign = TextAlign.Start)
+        Text(
+            text = "Analytics",
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Start
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        HistogramGraph(listOf(15f, 35f, 50f, 80f, 65f, 30f))
+
+        Card(modifier = Modifier.fillMaxWidth()) {
+            Row(
+                modifier = Modifier
+                    .padding(24.dp)
+                    .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.Top
+            ) {
+                Text(
+                    text = "Balance",
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Start,
+                    fontWeight = FontWeight.Medium
+                )
+                Row(
+                    verticalAlignment = Alignment.Top,
+                    horizontalArrangement = Arrangement.spacedBy(5.dp)
+                ) {
+                    Text("Mar - Aug")
+                    IconButton(onClick = {}, modifier = Modifier.offset(y = (-10).dp)) {
+                        Icon(
+                            imageVector = Icons.Filled.DateRange,
+                            contentDescription = "pick a date"
+                        )
+                    }
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp), horizontalArrangement = Arrangement.spacedBy(80.dp)
+            ) {
+                Column {
+                    Text(
+                        "Income",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier
+                            .alpha(0.8f)
+                            .padding(start = 10.dp)
+                    )
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text("|", color = Color(0xFF4CAF50), fontSize = 26.sp)
+                        Text(
+                            "32,000 Ush",
+                            style = MaterialTheme.typography.bodyLarge,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+                Column {
+                    Text(
+                        "Expense",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier
+                            .alpha(0.8f)
+                            .padding(start = 10.dp)
+                    )
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text("|", color = MaterialTheme.colorScheme.error, fontSize = 26.sp)
+                        Text(
+                            "12,000 Ush",
+                            style = MaterialTheme.typography.bodyLarge,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+            }
+        }
+
     }
 
 }
